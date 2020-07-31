a72604f51833148ae6e10d9ead7311bc8a2ffaaa
Yinchu Xia <larry.fantasy@gmail.com>
2020-07-31 02:12:25 -0700

    Updates

---
+++ precache-manifest.80a729c26ead81a5cd78ec87092d6021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56eecea7f7148cd4a8e2be53f47acf27",
+    "url": "/clusterpiwificonfig/index.html"
+  },
+  {
+    "revision": "2902f5055b78ca2af1b8",
+    "url": "/clusterpiwificonfig/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0f5c6db622fd1beb3c0",
+    "url": "/clusterpiwificonfig/static/js/2.e13b1f2d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clusterpiwificonfig/static/js/2.e13b1f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2902f5055b78ca2af1b8",
+    "url": "/clusterpiwificonfig/static/js/main.1fc43e02.chunk.js"
+  },
+  {
+    "revision": "f47e6a8f3e3fd466c937",
+    "url": "/clusterpiwificonfig/static/js/runtime-main.626d798f.js"
+  }
+]);
