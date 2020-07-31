3be2fa1c7623e36c22240960e9506616906c98f8
Yinchu Xia <larry.fantasy@gmail.com>
2020-07-31 02:00:35 -0700

    deploy

---
+++ docs/precache-manifest.98ccf5eb26bf59ecf00edb303473508b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba9c98477d84dbba3887bd5336dbbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cd1983f169d3a3960cf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2858d12b6c58b18be57",
+    "url": "/static/js/2.b2489176.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b2489176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd1983f169d3a3960cf",
+    "url": "/static/js/main.473e1aa8.chunk.js"
+  },
+  {
+    "revision": "cecd14e4305de061a80a",
+    "url": "/static/js/runtime-main.e4961c46.js"
+  }
+]);
